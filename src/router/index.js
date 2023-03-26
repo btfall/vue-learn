cfb893506dd3c99085944ad316cce70990b96caf
btfall <2229759485@qq.com>
2023-03-26 17:11:42 +0800

    3.26提交空项目

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+
+const routes = [
+  {
+    path: "/",
+    redirect:"login",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import( "../views/login/index.vue"),
+  },
+ 
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
